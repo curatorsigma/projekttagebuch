5ea6ce5c0851e46f5e65a34dcedc93da6e860992
Schleucher <jonathan.schleucher@acidresden.de>
2025-02-26 13:40:49 +0100

    update design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/landing/*.html", "./templates/*.html"],
+  content: ["./templates/landing/*.html", "./templates/*.html", "./templates/project/edit.html"],
   theme: {
     extend: {},
   },
